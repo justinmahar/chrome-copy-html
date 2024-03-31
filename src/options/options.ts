0a8ef721a442a5da0ac3057df7e79f43c453e9d3
Justin Mahar <contact@justinmahar.com>
2024-03-31 11:15:45 -0400

    Update options saved notification

--- src/options/options.ts
+++ src/options/options.ts
@@ -9,7 +9,7 @@ const saveOptions = () => {
     // Update status to let user know options were saved.
     const status = document.getElementById('status');
     if (status) {
-      status.textContent = '✅ Options saved.';
+      status.textContent = '✅ Options saved';
       setTimeout(() => {
         status.textContent = '';
       }, 2000);
